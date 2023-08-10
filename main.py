483ee03760f570ae516a21df441702f1f18f50e6
NahianShabab <nsnbd134@gmail.com>
2023-08-11 03:25:25 +0600

    added user,post,disaster,missing route

--- main.py
+++ main.py
@@ -95,5 +95,75 @@ app = FastAPI()
 #     return missing_crud.update_missing(db,missing_person_id,missing,current_user)
 #     pass
 
+
+# User CRUD
+@app.post('/users/', tags=['Users'], summary="Create a new user", response_model=schemas.User_Out)
+async def create_user(user: schemas.User_Create_Update, db: Session = Depends(get_db)):
+    pass
+
+@app.get('/users/', tags=['Users'], summary="Get a list of users", response_model=list[schemas.User_Out])
+async def list_users(db: Session = Depends(get_db)):
+    pass
+
+@app.get('/users/{user_id}', tags=['Users'], summary="Get user by ID", response_model=schemas.User_Out)
+async def read_user(user_id: int, db: Session = Depends(get_db)):
+    pass
+
+@app.put('/users/{user_id}', tags=['Users'], summary="Update user by ID", response_model=schemas.User_Out)
+async def update_user(user_id: int, user: schemas.User_Create_Update, db: Session = Depends(get_db)):
+    pass
+
+# Post CRUD
+@app.post('/posts/', tags=['Posts'], summary="Create a new post", response_model=schemas.Post)
+async def create_post(post: schemas.Post_Create_Update, db: Session = Depends(get_db)):
+    pass
+
+@app.get('/posts/', tags=['Posts'], summary="Get a list of posts", response_model=list[schemas.Post])
+async def list_posts(db: Session = Depends(get_db)):
+    pass
+
+@app.get('/posts/{post_id}', tags=['Posts'], summary="Get post by ID", response_model=schemas.Post)
+async def read_post(post_id: int, db: Session = Depends(get_db)):
+    pass
+
+@app.put('/posts/{post_id}', tags=['Posts'], summary="Update post by ID", response_model=schemas.Post)
+async def update_post(post_id: int, post: schemas.Post_Create_Update, db: Session = Depends(get_db)):
+    pass
+
+# Disaster CRUD
+@app.post('/disasters/', tags=['Disasters'], summary="Create a new disaster", response_model=schemas.Disaster)
+async def create_disaster(disaster: schemas.Disaster_Create, db: Session = Depends(get_db)):
+    pass
+
+@app.get('/disasters/', tags=['Disasters'], summary="Get a list of disasters", response_model=list[schemas.Disaster])
+async def list_disasters(db: Session = Depends(get_db)):
+    pass
+
+@app.get('/disasters/{disaster_id}', tags=['Disasters'], summary="Get disaster by ID", response_model=schemas.Disaster)
+async def read_disaster(disaster_id: int, db: Session = Depends(get_db)):
+    pass
+
+@app.put('/disasters/{disaster_id}', tags=['Disasters'], summary="Update disaster by ID", response_model=schemas.Disaster)
+async def update_disaster(disaster_id: int, disaster: schemas.Disaster_Update, db: Session = Depends(get_db)):
+    pass
+
+# Missing Person CRUD
+@app.post('/missing/', tags=['Missing Person'], summary="Create a new missing person report", response_model=schemas.Missing_Person)
+async def create_missing(missing: schemas.Missing_Person_Create, db: Session = Depends(get_db)):
+    pass
+
+@app.get('/missing/', tags=['Missing Person'], summary="Get a list of missing persons", response_model=list[schemas.Missing_Person])
+async def list_missing_persons(db: Session = Depends(get_db)):
+    pass
+
+@app.get('/missing/{missing_person_id}', tags=['Missing Person'], summary="Get missing person by ID", response_model=schemas.Missing_Person)
+async def read_missing(missing_person_id: int, db: Session = Depends(get_db)):
+    pass
+
+@app.put('/missing/{missing_person_id}', tags=['Missing Person'], summary="Update missing person by ID", response_model=schemas.Missing_Person)
+async def update_missing(missing_person_id: int, missing: schemas.Missing_Person_Update, db: Session = Depends(get_db)):
+    pass
+
+
 if __name__=="__main__":
     uvicorn.run("main:app",port=4001,reload=True)
